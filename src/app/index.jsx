ceb92204dac30fb196c20afcf8df04fcd3dbbeb0
Bhagat Singh <bhagatsinghswe@gmail.com>
2026-04-19 23:38:50 +0530

    Part - 4, Hotel Search and Axios added

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -4,15 +4,17 @@ import Header from '@/components/layouts/header.layout'
 import Footer from '@/components/layouts/footer.layout'
 import HotelDetails from './hotel-details'
 import {SignInPage, SignUpPage} from './auth'
+import SearchPage from './search'
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Home />
+      {/* <Home /> */}
       {/* <HotelDetails /> */}
       {/* <SignInPage /> */}
       {/* <SignUpPage /> */}
+      <SearchPage />
       <Footer />
     </div>
   )
